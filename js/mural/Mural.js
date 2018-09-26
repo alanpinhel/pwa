4902fc04230db7fad0e67b8307536cff4713adfd
Alan Pinhel <alanpinhel@gmail.com>
2018-09-26 14:58:20 -0300

    removendo e editando cartões no localstorage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -2,12 +2,23 @@ const Mural = (function(_render, Filtro){
     "use strict"
     let cartoes = (JSON.parse(localStorage.getItem('cartoes')) || [])
         .map(({conteudo, tipo}) => new Cartao(conteudo, tipo))
-    
-    const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    cartoes.forEach(preparaCartao)
 
+    const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+  
     render()
 
     Filtro.on("filtrado", render)
+  
+    function preparaCartao(cartao) {
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", () => {
+            cartoes = [].concat(cartoes)
+            cartoes.splice(cartoes.indexOf(cartao), 1)
+            salvaCartoes()
+            render()
+        })
+    }
 
     function salvaCartoes() {
         localStorage.setItem('cartoes', JSON.stringify(cartoes.map(
@@ -22,12 +33,8 @@ const Mural = (function(_render, Filtro){
         
         cartoes.push(cartao)
         salvaCartoes()
-        cartao.on("mudanca.**", render)
-        cartao.on("remocao", ()=>{
-            cartoes = cartoes.slice(0)
-            cartoes.splice(cartoes.indexOf(cartao),1)
-            render()
-        })
+        cartao.on("mudanca.**", () => render)
+        preparaCartao(cartao)
         render()
         return true
     }

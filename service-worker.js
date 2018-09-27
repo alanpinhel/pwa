32a2cdb1ecea56164894047500117468c847f46f
Alan Pinhel <alanpinhel@gmail.com>
2018-09-27 10:51:51 -0300

    pegando todas as respostas do service worker

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,6 @@ self.addEventListener('fetch', function(event) {
   let pedido = event.request
   let promiseResposta = caches.open('ceep-imagens')
     .then(cache => cache.match(pedido))
-    .then(res => res)
+    .then(res => res || fetch(pedido))
   event.respondWith(promiseResposta)
 })
